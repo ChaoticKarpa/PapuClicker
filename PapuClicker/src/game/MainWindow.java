df4d7a930c6d61e6e035bab6cbf6b7ac2d373b27
ChaoticKarpa <mondarizalex2@gmail.com>
2023-04-14 12:28:19 +0200

    PapuClicker THE INCREMENTAL UPDATE (VA REGULAR XDDDDD)

--- PapuClicker/src/game/MainWindow.java
+++ PapuClicker/src/game/MainWindow.java
@@ -16,7 +16,7 @@ import javax.swing.JOptionPane;
  * @author ChaoticKarpa
  */
 public class MainWindow extends javax.swing.JFrame {
-    
+
     private int number = 0;
 
     /**
@@ -38,7 +38,7 @@ public class MainWindow extends javax.swing.JFrame {
             e.printStackTrace();
         }
     }
-    
+
     public void loadFile() {
         try {
             FileReader reader = new FileReader("papusave.dat");
@@ -47,6 +47,19 @@ public class MainWindow extends javax.swing.JFrame {
             System.out.println("Archivo cargado correctamente.");
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public void autoIncrement() {
+        while (true) {
+            try {
+                int num = Integer.parseInt(counter.getText()) + 1;
+                counter.setText(String.valueOf(num));
+                Thread.sleep(500); // ralentiza la ejecución en 1 segundo
+                
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -67,10 +80,6 @@ public class MainWindow extends javax.swing.JFrame {
         jMenuItem7 = new javax.swing.JMenuItem();
         jMenuItem8 = new javax.swing.JMenuItem();
         jMenu1 = new javax.swing.JMenu();
-        jMenuItem2 = new javax.swing.JMenuItem();
-        jMenuItem3 = new javax.swing.JMenuItem();
-        jMenuItem4 = new javax.swing.JMenuItem();
-        jMenuItem5 = new javax.swing.JMenuItem();
         jMenuItem6 = new javax.swing.JMenuItem();
         Ayuda = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
@@ -118,39 +127,8 @@ public class MainWindow extends javax.swing.JFrame {
 
         jMenu1.setText("Tienda");
 
-        jMenuItem2.setText("Papu Gaming");
-        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem2ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem2);
-
-        jMenuItem3.setText("Papu Pro");
-        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem3ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem3);
-
-        jMenuItem4.setText("Papu Pro Gaming");
-        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem4ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem4);
-
-        jMenuItem5.setText("Papu Lince");
-        jMenuItem5.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem5ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem5);
-
-        jMenuItem6.setText(":v");
+        jMenuItem6.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_V, java.awt.event.InputEvent.CTRL_DOWN_MASK));
+        jMenuItem6.setText(":v (1000 PapuPuntos)");
         jMenuItem6.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem6ActionPerformed(evt);
@@ -223,24 +201,21 @@ public class MainWindow extends javax.swing.JFrame {
         JOptionPane.showMessageDialog(null, "https://github.com/ChaoticKarpa/PapuClicker", "Github", JOptionPane.INFORMATION_MESSAGE);
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
-    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jMenuItem2ActionPerformed
-
-    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jMenuItem3ActionPerformed
-
-    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jMenuItem4ActionPerformed
-
-    private void jMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem5ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jMenuItem5ActionPerformed
-
     private void jMenuItem6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem6ActionPerformed
-        // TODO add your handling code here:
+
+        int countNum = Integer.parseInt(counter.getText());
+
+        if (countNum >= 1000) {
+            int subtract = countNum - 1000;
+            counter.setText(String.valueOf(subtract));
+            new Thread(new Runnable() {
+            public void run() {
+                autoIncrement();
+            }
+        }).start();
+        }else{
+            System.out.println("Dinero insuficiente");
+        }
     }//GEN-LAST:event_jMenuItem6ActionPerformed
 
     private void jMenuItem7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem7ActionPerformed
@@ -304,6 +279,8 @@ public class MainWindow extends javax.swing.JFrame {
                     + " Puede que en esta carpeta no tengas permisos de escritura.");
             e.printStackTrace();
         }
+        
+        
 
     }
 
@@ -316,10 +293,6 @@ public class MainWindow extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItem3;
-    private javax.swing.JMenuItem jMenuItem4;
-    private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JMenuItem jMenuItem6;
     private javax.swing.JMenuItem jMenuItem7;
     private javax.swing.JMenuItem jMenuItem8;
